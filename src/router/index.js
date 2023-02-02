9ac6881caa2ade652b07abe34ba30c27f81de2d3
AmirHekmat79 <amir.hekmat1379@gmail.com>
2023-02-02 13:16:50 +0330

    AddTables

--- src/router/index.js
+++ src/router/index.js
@@ -2,26 +2,66 @@ import Vue from "vue";
 import VueRouter from "vue-router";
 // import LoginForm from "../views/LoginForm.vue";
 import DashboardPannel from "../views/DashboardPannel.vue";
+import UsersPage from "../views/UsersPage.vue";
+import LicensePage from "../views/LicensePage.vue";
+import ProjectsPage from "../views/ProjectsPage.vue";
+import CustomerPage from "../views/CustomerPage.vue";
+import auth from "../middleware/auth";
 // import App from "../App.vue";
-import App from "../App.vue";
+// import App from "../App.vue";
 Vue.use(VueRouter);
 
 const routes = [
   {
     path: "/Login",
     name: "Login",
-   component: () => import('../views/LoginForm.vue'),
-   meta: {
-    hideNavbar: true,
-   }
+    component: () => import("../views/LoginForm.vue"),
+    meta: {
+      hideNavbar: true,
+      middleware: auth,
+    },
   },
   {
-    path:"/",
-    name:"home" ,
-    component : App ,
-    children:[{path:"Dashboard" , name:"Dashboard" , component :DashboardPannel } ,
-  ]
-  }
+    path: "/",
+    name: "home",
+    component: DashboardPannel,
+    meta: {
+      middleware: auth,
+    },
+  },
+  {
+    path: "/Users",
+    name: "Users",
+    component: UsersPage,
+    meta: {
+      middleware: auth,
+    },
+  },
+  {
+    path: "/Licenses",
+    name: "License",
+    component: LicensePage,
+    meta: {
+      middleware: auth,
+    },
+  },
+  {
+    path: "/Projects",
+    name: "Projects",
+    component: ProjectsPage,
+    meta: {
+      middleware: auth,
+    },
+  },
+
+  {
+    path: "/Customers",
+    name: "Customers",
+    component: CustomerPage,
+    meta: {
+      middleware: auth,
+    },
+  },
 ];
 
 const router = new VueRouter({
@@ -30,3 +70,12 @@ const router = new VueRouter({
 });
 
 export default router;
+
+// router.beforeEach((to, from, next) => {
+//   const loggedIn = localStorage.getItem("content_superz");
+
+//   if (to.matched.some((record) => record.meta.requiresAuth) && !loggedIn) {
+//     next("/login");
+//   }
+//   next();
+// });
--- src/views/DashboardPannel.vue
+++ src/views/DashboardPannel.vue
@@ -1,31 +1,73 @@
 <template>
-  <div class="mt-20">
-    <highcharts
+  <v-container class="fluid mt-20">
+    <!-- <highcharts
       :constructor-type="'stockChart'"
       :options="chartOptions"
-    ></highcharts>
-  </div>
+    ></highcharts> -->
+    <v-row>
+      <v-col v-for="card in cards" :key="card">
+        <v-card class="mx-auto" max-width="344">
+          <v-img
+            src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg"
+            height="200px"
+          ></v-img>
+
+          <v-card-title> {{ card.title }} </v-card-title>
+
+          <v-card-subtitle> {{ card.subtitle }}</v-card-subtitle>
+
+          <v-card-actions>
+            <v-btn color="orange lighten-2" text> {{ card.btnText }} </v-btn>
+
+            <v-spacer></v-spacer>
+
+            <v-icon> {{ card.iconTitle }}</v-icon>
+          </v-card-actions>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 <script>
-import { Chart } from "highcharts-vue";
-import Highcharts from "highcharts";
-import stockInit from "highcharts/modules/stock";
+// import { Chart } from "highcharts-vue";
+// import Highcharts from "highcharts";
+// import stockInit from "highcharts/modules/stock";
 
-stockInit(Highcharts);
+// stockInit(Highcharts);
 
 export default {
   name: "DashboardPannel",
 
   data: () => ({
     //
-    chartOptions: [
+    // chartOptions: [
+    //   {
+    //     data: [1, 2, 3], // sample data
+    //   },
+    // ],
+    cards: [
       {
-        data: [1, 2, 3], // sample data
+        title: "Top western road trips",
+        subtitle: "1,000 miles of wonder",
+        btnText: "Explore",
+        iconTitle: "mdi-account-box",
+      },
+      {
+        title: "Top western road trips",
+        subtitle: "1,000 miles of wonder",
+        btnText: "Explore",
+        iconTitle: "mdi-account-box",
+      },
+      {
+        title: "Top western road trips",
+        subtitle: "1,000 miles of wonder",
+        btnText: "Explore",
+        iconTitle: "mdi-account-box",
       },
     ],
   }),
   components: {
-    highcharts: Chart,
+    // highcharts: Chart,
   },
 };
 </script>
