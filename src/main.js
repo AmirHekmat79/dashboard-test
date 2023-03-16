2c9e904ad67879ce9b169b564f87bf58bfb2fe6c
AmirHekmat79 <amir.hekmat1379@gmail.com>
2023-03-16 18:25:36 +0330

    Add Charts

--- src/main.js
+++ src/main.js
@@ -2,11 +2,8 @@ import Vue from 'vue';
 import App from './App.vue';
 import vuetify from './plugins/vuetify';
 import router from './router';
-import HighchartsVue from 'highcharts-vue';
-
-// import LoginForm from "./components/LoginForm.vue";
 Vue.config.productionTip = false;
-Vue.use(HighchartsVue);
+
 
 new Vue({
   vuetify,
--- src/views/DashboardPannel.vue
+++ src/views/DashboardPannel.vue
@@ -1,73 +1,162 @@
 <template>
   <v-container class="fluid mt-20">
-    <!-- <highcharts
-      :constructor-type="'stockChart'"
-      :options="chartOptions"
-    ></highcharts> -->
-    <v-row>
-      <v-col v-for="card in cards" :key="card">
-        <v-card class="mx-auto" max-width="344">
-          <v-img
-            src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg"
-            height="200px"
-          ></v-img>
-
-          <v-card-title> {{ card.title }} </v-card-title>
-
-          <v-card-subtitle> {{ card.subtitle }}</v-card-subtitle>
-
-          <v-card-actions>
-            <v-btn color="orange lighten-2" text> {{ card.btnText }} </v-btn>
-
-            <v-spacer></v-spacer>
-
-            <v-icon> {{ card.iconTitle }}</v-icon>
-          </v-card-actions>
+    <v-row class="d-flex justify-center items-center flex-wrap">
+      <v-col col="4" v-for="card in cards" :key="card.item">
+        <v-card class="align-center">
+          <v-icon class="Margin1 pt-3" :color="card.Color">{{
+            card.icon
+          }}</v-icon>
+          <v-card-title class="py-3 font-weight-bold Margin2">{{
+            card.counts
+          }}</v-card-title>
+          <v-card-subtitle class="Margin3">
+            {{ card.item }}
+          </v-card-subtitle>
+        </v-card>
+      </v-col>
+    </v-row>
+    <v-row class="d-flex justify-center items-center flex-wrap">
+      <v-col col="4">
+        <v-card class="elevation-4 text-center">
+          <v-card-title class="font-weight-bold">وضعیت مشتری ها</v-card-title>
+          <ColumnChart :xAxis="chart.xAxis" :series="chart.customers" />
+        </v-card>
+      </v-col>
+      <v-col col="4">
+        <v-card class="elevation-4">
+          <v-card-title class="font-weight-bold">وضعیت پروژه ها</v-card-title>
+          <LineChart :xAxis="chart.xAxis" :series="chart.projects" />
+        </v-card>
+      </v-col>
+      <v-col col="4">
+        <v-card class="elevation-4">
+          <v-card-title class="font-weight-bold">وضعیت کاربران </v-card-title>
+          <ColumnChart :xAxis="chart.xAxis" :series="chart.users" />
         </v-card>
       </v-col>
     </v-row>
   </v-container>
 </template>
 <script>
-// import { Chart } from "highcharts-vue";
+// import { Chart } from 'highcharts-vue';
+// import Highcharts from 'highcharts';
+// import stockInit from 'highcharts/modules/stock';
+
+// stockInit(Highcharts);
+
 // import Highcharts from "highcharts";
 // import stockInit from "highcharts/modules/stock";
 
 // stockInit(Highcharts);
-
+import { ColumnChart } from 'vuejs-highcharts';
+import { LineChart } from 'vuejs-highcharts';
 export default {
-  name: "DashboardPannel",
-
-  data: () => ({
-    //
-    // chartOptions: [
-    //   {
-    //     data: [1, 2, 3], // sample data
-    //   },
-    // ],
-    cards: [
-      {
-        title: "Top western road trips",
-        subtitle: "1,000 miles of wonder",
-        btnText: "Explore",
-        iconTitle: "mdi-account-box",
-      },
-      {
-        title: "Top western road trips",
-        subtitle: "1,000 miles of wonder",
-        btnText: "Explore",
-        iconTitle: "mdi-account-box",
-      },
-      {
-        title: "Top western road trips",
-        subtitle: "1,000 miles of wonder",
-        btnText: "Explore",
-        iconTitle: "mdi-account-box",
-      },
-    ],
-  }),
+  name: 'DashboardPannel',
   components: {
-    // highcharts: Chart,
+    ColumnChart,
+    LineChart,
+  },
+  data() {
+    return {
+      chart: {
+        title: 'Area chart title',
+        subtitle: 'Area chart subtitle',
+        backgroundColor: '#eee',
+        xAxis: {
+          // config here
+          categories: [2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007],
+        },
+        users: [
+          // config here
+          {
+            name: 'کاربران فعال',
+            color: '#2f7ed8',
+            data: [43934, 52503, 57177, 69658, 97031, 119931, 137133, 154175],
+          },
+          {
+            name: 'کاربران غیر فعال',
+            color: '#0d233a',
+            data: [24916, 24064, 29742, 29851, 32490, 30282, 38121, 40434],
+          },
+        ],
+        projects: [
+          // config here
+          {
+            name: 'پروژه فعال',
+            color: '#03bbd4',
+            data: [43934, 52503, 57177, 69658, 97031, 119931, 137133, 154175],
+          },
+          {
+            name: 'پروژه غیر فعال',
+            color: '#f7a35c',
+            data: [24916, 24064, 29742, 29851, 32490, 30282, 38121, 40434],
+          },
+        ],
+        customers: [
+          // config here
+          {
+            name: 'مشتری ها ',
+            color: '#03bbd4',
+            data: [43934, 52503, 57177, 69658, 97031, 119931, 137133, 154175],
+          },
+          // {
+          //   name: 'پروژه غیر فعال',
+          //   color: '#f7a35c',
+          //   data: [24916, 24064, 29742, 29851, 32490, 30282, 38121, 40434],
+          // },
+        ],
+        //   config: {
+        //     colors: ['#2f7ed8', '#0d233a'],
+        //     credits: {
+        //       enabled: true
+        //     },
+        //     // caption: {
+        //     //   text: '<b>The caption renders in the bottom, and is part of the exported chart.</b>'
+        //     // }
+        // },
+      },
+
+      cards: [
+        {
+          item: 'پیام ها',
+          icon: 'mdi-bell',
+          counts: 853,
+          Color: 'warning',
+        },
+        {
+          item: 'سود امروز',
+          icon: 'mdi-bank',
+          counts: '$' + 3521,
+          Color: 'success',
+        },
+        {
+          item: 'مشتری ها',
+          icon: 'mdi-account',
+          counts: 562,
+          Color: 'primary',
+        },
+        {
+          item: 'پشتیبانی',
+          icon: 'mdi-heart',
+          counts: 353,
+          Color: 'red',
+        },
+      ],
+    };
   },
 };
 </script>
+<style scoped>
+.Margin1 {
+  /* text-align: center; */
+  margin-right: 130px;
+}
+.Margin2 {
+  /* text-align: center; */
+  margin-right: 114px;
+}
+.Margin3 {
+  /* text-align: center; */
+  margin-right: 110px;
+}
+</style>
