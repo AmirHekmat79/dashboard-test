3d5e8c32f9691a68db7d5af756d6a28a13b907ee
S.Mohammad Hosseininejad <smohammadhnd@gmail.com>
2023-03-18 21:35:20 +0330

    feat(vuetify.js): change vuetify locale to fa

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,26 +1,31 @@
-import Vue from 'vue';
-import Vuetify from 'vuetify/lib/framework';
+import Vue from "vue";
+import Vuetify from "vuetify/lib/framework";
+import fa from "vuetify/es5/locale/fa";
 // import './assets/css/style.css';
 Vue.use(Vuetify);
 
 export default new Vuetify({
+  lang: {
+    locales: { fa },
+    current: "fa",
+  },
   icons: {
-    iconfont: 'mdi' || 'md' || 'fa' || 'fa4',
+    iconfont: "mdi" || "md" || "fa" || "fa4",
   },
   rtl: true,
   breakpoint: {
-    mobileBreakpoint: 'sm',
+    mobileBreakpoint: "sm",
   },
   // defaultassets: {
   //   font: false,
   // },
   theme: {
-    defaultTheme: 'dark',
-    dark : {
-      primary : '#2980b9'
-    } ,
-    light : {
-      primary : '#ffffff'
-    }
+    defaultTheme: "dark",
+    dark: {
+      primary: "#2980b9",
+    },
+    light: {
+      primary: "#ffffff",
+    },
   },
 });
